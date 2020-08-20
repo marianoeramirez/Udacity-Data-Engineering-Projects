db07d16cbd821f1ea707177cf93de5b6569908ea
bomba1990 <marianoramirez353@gmail.com>
2020-08-20 17:43:20 -0300

    Data modeling postgres initial version

---
+++ Data Modeling Postgres/sql_queries.py
@@ -0,0 +1,78 @@
+
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
+
+# CREATE TABLES
+
+songplay_table_create = ("""
+CREATE TABLE songplays(
+songplay_id serial PRIMARY KEY, 
+start_time bigint, 
+user_id int, 
+level varchar, 
+song_id varchar, 
+artist_id varchar, 
+session_id int, 
+location varchar, 
+user_agent varchar
+)
+""")
+
+
+user_table_create = ("""CREATE TABLE users(
+user_id int PRIMARY KEY, first_name varchar, last_name varchar, gender char(1), level varchar
+) """)
+
+song_table_create = ("""CREATE TABLE songs(
+song_id varchar, title varchar, artist_id varchar, year int, duration float
+) """)
+
+artist_table_create = ("""CREATE TABLE  artists(
+artist_id varchar, name varchar, location varchar, latitude float, longitude float
+) """)
+
+time_table_create = ("""CREATE TABLE time(
+start_time bigint PRIMARY KEY , hour int, day int, week int, month int, year int, weekday int
+)""")
+
+# INSERT RECORDS
+
+songplay_table_insert = (""" INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+""")
+
+user_table_insert = ("""INSERT INTO users (user_id, first_name, last_name, gender, level) 
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING
+""")
+
+song_table_insert = ("""INSERT INTO songs (song_id , title , artist_id , year , duration) 
+VALUES (%s, %s, %s, %s, %s)
+""")
+
+artist_table_insert = ("""INSERT INTO artists (artist_id  , name , location , latitude, longitude) 
+VALUES (%s, %s, %s, %s, %s)
+""")
+
+
+time_table_insert = ("""INSERT INTO time (start_time  , hour , day , week , month , year , weekday) 
+VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING
+""")
+
+# FIND SONGS
+
+song_select = ("""
+SELECT s.song_id, a.artist_id FROM songs s
+JOIN artists a on a.artist_id = s.artist_id
+where s.title = %s and a.name = %s and s.duration = %s
+""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
