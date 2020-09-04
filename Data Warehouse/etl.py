8632b5f5cb1e5ce6d71a8c343aa33712ffa94ccb
bomba1990 <marianoramirez353@gmail.com>
2020-09-04 10:12:39 -0300

    Comments of functions

--- Data Warehouse/etl.py
+++ Data Warehouse/etl.py
@@ -4,18 +4,29 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Load the staging tables in the database using the queries in `copy_table_queries` list.
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Load the production tables in the database using the queries in `insert_table_queries` list.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Main function of the applicattion
+    - load the staging tables in the database
+    - Load the production tables with the information from database
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
