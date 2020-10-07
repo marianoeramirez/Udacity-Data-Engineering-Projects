b9c93dcefe4be6c4e257af2608264aa9c3ec6e1a
marianoeramirez <Sosinformatico1990>
2020-10-07 08:48:52 -0300

    airflow first commit

---
+++ Airflow/plugins/__init__.py
@@ -0,0 +1,18 @@
+from airflow.plugins_manager import AirflowPlugin
+
+from . import operators
+from . import helpers
+
+
+# Defining the plugin class
+class UdacityPlugin(AirflowPlugin):
+    name = "udacity_plugin"
+    operators = [
+        operators.StageToRedshiftOperator,
+        operators.LoadFactOperator,
+        operators.LoadDimensionOperator,
+        operators.DataQualityOperator
+    ]
+    helpers = [
+        helpers.SqlQueries
+    ]
---
+++ Airflow/plugins/operators/stage_redshift.py
@@ -0,0 +1,50 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+
+class StageToRedshiftOperator(BaseOperator):
+    ui_color = '#358140'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id: str = "",
+                 aws_credential_id: str = "",
+                 table_name: str = "",
+                 s3_bucket: str = "",
+                 s3_key: str = "",
+                 file_format: str = "",
+                 log_json_file: str = "",
+                 *args, **kwargs):
+
+        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credential_id = aws_credential_id
+        self.table_name = table_name
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.file_format = file_format
+        self.log_json_file = log_json_file
+        self.execution_date = kwargs.get('execution_date')
+
+    def execute(self, context):
+        aws_hook = AwsHook(self.aws_credential_id)
+        credentials = aws_hook.get_credentials()
+
+        base_copy_query = " COPY {} FROM '{}' ACCESS_KEY_ID '{}' SECRET_ACCESS_KEY '{}' FORMAT AS json '{}';"
+
+        s3_path = f"s3://{self.s3_bucket}/{self.s3_key}"
+
+        if self.log_json_file != "":
+            self.log_json_file = f"s3://{self.s3_bucket}/{self.log_json_file}"
+            copy_query = base_copy_query.format(self.table_name, s3_path, credentials.access_key,
+                                                credentials.secret_key, self.log_json_file)
+        else:
+            copy_query = base_copy_query.format(self.table_name, s3_path, credentials.access_key,
+                                                credentials.secret_key, 'auto')
+
+        self.log.info(f"Running copy: {copy_query}")
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        redshift_hook.run(copy_query)
+        self.log.info(f"Table {self.table_name} ready!")
